5bef13e5afab406661d4b687f713f0674d2c0db4
AlexKinn <alex.kn@live.com>
2023-07-19 17:40:03 +0300

    create main display buttons

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,29 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Home.module.scss";
 import SidebarItem from "./SidebarItem";
-import { List, ListItem } from "@mui/material";
+import { Card, CardActionArea, CardMedia, IconButton, List, ListItem } from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+
+
+const topPosts = [
+    
+    {
+        poster: "witcherPoster.jpg",
+        image: "witcherIMG.jpg",
+        mainText: "The Witcher",
+        secondaryText: "Watch the New Season 3 Trailer"
+    },
+    {   
+        poster: "oppenheimerPoster.jpg",
+        image: "oppenheimerIMG.jpg",
+        mainText: "5 Things to Watch Now",
+        secondaryText: "Here's What's Hot This Week"
+    }, 
+    {
+        poster: "wonkaPoster.jpg",
+        image: "wonkaIMG.jpg",
+        mainText: "Timothée Chalamet Stars in 'Wonka'",
+        secondaryText: "Watch the First Trailer"
+    }
+];
+
 
 
 export default function Home() {
+    const [selectedPostIndex, setSelectedPostIndex] = useState(0);
+    
+    const nextPost = () => {
+        if(selectedPostIndex == topPosts.length-1) {
+            setSelectedPostIndex(0);
+        }
+        else {
+            setSelectedPostIndex(selectedPostIndex+1);
+        }
+    }
+    const previousPost = () => {
+        if(selectedPostIndex == 0) {
+            setSelectedPostIndex(topPosts.length-1);
+        }
+        else {
+            setSelectedPostIndex(selectedPostIndex-1);
+        }
+    }
+    const renderSidebarItems = () => {    
+        topPosts.map((post) => {
+            // if(index === selectedPostIndex) {
+            //     return null;
+            // }
+            return(
+                <SidebarItem 
+                    key={post.mainText}
+                    image={post.image}   
+                    mainText={post.mainText}
+                    secondaryText={post.secondaryText}
+                />
+            )
+        })
 
+
+        // const renderedList = [];
+        // for(let i=0; i<topPosts.length; i++) {
+        //     if(i === selectedPostIndex) {
+        //         continue;
+        //     }
+        //     renderedList.add(
+        //         <SidebarItem 
+        //                 key={post.mainText}
+        //                 image={post.image}   
+        //                 mainText={post.mainText}
+        //                 secondaryText={post.secondaryText}
+        //             />
+        //     )   
+        // }
+    };
+    
     
     return(
         <div className={classes.home}>
             <div className={classes.home__top}>
-                <div className={classes.home__top__display}>
-                    <a>
-                        <img src="https://m.media-amazon.com/images/M/MV5BNjMxYjViZWYtZWZmNy00ZjVmLWJjMzAtNTIxNTFkZTRmMmU4XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._CR7,159,2124,1195_QL75_UX500_CR0,0,500,281_.jpg" 
-                        alt="display image" className={classes.home__top__display__img}/>
-                    </a>
-                </div>
-                <List className={classes.home__top__sidebar}>
-                    <ListItem className={classes.home__top__sidebar__sidebarItem}>
-                        <SidebarItem 
-                            image={"img1.jpg"}
-                            mainText={"5 Things to Watch Now"}
-                            secondaryText={"Here's What's Hot This Week"}
-                        />                        
-                    </ ListItem>
+                <Card className={classes.home__top__display}>
+                    <CardActionArea className={classes.home__top__display__imgContainer}>
+                        <CardMedia className={classes.home__top__display__imgContainer__img}
+                            component="img"
+                            image={topPosts[selectedPostIndex].poster}
+                            alt="mainImage"
+                        />
+                    </CardActionArea>
+                    <IconButton aria-label="previous option"
+                        className={`${classes.home__top__display__backButton} 
+                                    ${classes.home__top__display__directionalButton}`}
+                        onClick={previousPost}>
+                        <ArrowBackIos />
+                    </IconButton>
+                    <IconButton aria-label="next option" 
+                        className={`${classes.home__top__display__forwardButton} 
+                                    ${classes.home__top__display__directionalButton}`}
+                        onClick={nextPost} >
+                        <ArrowForwardIos />
+                    </IconButton>
+                </Card>
+                <List>
+                    {renderSidebarItems}
                 </List>
             </div>
         </div>
--- app/src/pages/Home/SidebarItem.tsx
+++ app/src/pages/Home/SidebarItem.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import classes from "./SidebarItem.module.scss";
-import { Card, CardContent, CardMedia, CardActionArea, Typography } from '@mui/material';
+import { ListItem, Card, CardContent, CardMedia, CardActionArea, Typography, Box } from '@mui/material';
 
 function SidebarItem(props : any) {
  
     return(
-        <Card className={classes.sideCard}>
-            <CardActionArea>
-                <CardMedia className={classes.sideCard__img}
-                    component="img"
-                    image={ props.image }
-                    alt={ props.mainText }
-                />
-                <CardContent>
-                    <Typography className={classes.sideCard__mainText}>{props.mainText}</Typography>
-                    <Typography className={classes.sideCard__secondaryText}>{props.secondaryText}</Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <ListItem className={classes.sidebarItem} key={ props.key }>
+            <Card>
+                <CardActionArea>
+                    <div className={classes.sidebarItem__card}>
+                        <CardMedia className={classes.sidebarItem__card__img}
+                            component="img"
+                            image={ props.image }
+                            alt={ props.mainText }
+                        />
+                        <CardContent>
+                            <Typography className={classes.sidebarItem__card__mainText}>
+                                {props.mainText}
+                            </Typography>
+                            <Typography className={classes.sidebarItem__card__secondaryText}>
+                                {props.secondaryText}
+                            </Typography>
+                        </CardContent>
+                    </div>
+                </CardActionArea>
+            </Card>
+        </ListItem>
     )
 }
 
