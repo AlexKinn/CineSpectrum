a79d7c5f82ab04eaf4159acf4f5821f30d01fd19
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-11-30 14:17:59 +0000

    setup GetTrendingMedia route

---
+++ server/CineSpectrum.API/Controllers/MediaController.cs
@@ -0,0 +1,24 @@
+﻿using CineSpectrum.API.Models.DTOs;
+using CineSpectrum.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineSpectrum.API.Controllers;
+
+[ApiController]
+public class MediaController : ControllerBase
+{
+    private readonly IMediaService _mediaService;
+
+    public MediaController(IMediaService MediaService)
+    {
+        _mediaService = MediaService;
+    }
+
+    [HttpGet("trending-media")]
+    public IActionResult GetTrendingMedia()
+    {
+        List<MediaExternalApiDto> trendingMedia = _mediaService.GetTrendingMedia();
+
+        return Ok(trendingMedia);
+    }
+}
