0ed274fd82ba991eb9d48cfbca7e0e4efacb0bf9
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-04 12:03:31 +0000

    setup a DB context for media

---
+++ server/CineSpectrum.API/Data/MediaContext.cs
@@ -0,0 +1,20 @@
+﻿using CineSpectrum.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineSpectrum.API.Data
+{
+    public class MediaContext : DbContext
+    {
+        public MediaContext(DbContextOptions<MediaContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost;Database=cinespectrumdb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
