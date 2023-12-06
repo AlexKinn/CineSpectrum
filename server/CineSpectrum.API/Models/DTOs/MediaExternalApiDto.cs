662ac63532f3b161eb09f2e9e0053793e4d5e0b4
AlexKinn <alex.kn@live.com>
2023-12-06 16:03:30 +0000

    fix JSON serialization for model

--- server/CineSpectrum.API/Models/DTOs/MediaExternalApiDto.cs
+++ server/CineSpectrum.API/Models/DTOs/MediaExternalApiDto.cs
@@ -1,24 +1,60 @@
-﻿namespace CineSpectrum.API.Models.DTOs;
+﻿using System.Text.Json.Serialization;
+
+namespace CineSpectrum.API.Models.DTOs;
 
 public class MediaExternalApiDto
 {
+    [JsonPropertyName("adult")]
     public bool Adult { get; set; }
+
+    [JsonPropertyName("backdrop_path")]
     public string BackdropPath { get; set; }
+
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+
+    [JsonPropertyName("title")]
     public string? Title { get; set; }
+
+    [JsonPropertyName("name")]
     public string? Name { get; set; }
+
+    [JsonPropertyName("original_language")]
     public string OriginalLanguage { get; set; }
+
+    [JsonPropertyName("original_title")]
     public string? OriginalTitle { get; set; }
+
+    [JsonPropertyName("original_name")]
     public string? OriginalName { get; set; }
+
+    [JsonPropertyName("overview")]
     public string Overview { get; set; }
+
+    [JsonPropertyName("poster_path")]
     public string PosterPath { get; set; }
+
+    [JsonPropertyName("media_type")]
     public string MediaType { get; set; }
+
+    [JsonPropertyName("genre_ids")]
     public List<int> GenreIds { get; set; }
+
+    [JsonPropertyName("popularity")]
     public double Popularity { get; set; }
+
+    [JsonPropertyName("release_date")]
     public string? ReleaseDate { get; set; }
+
+    [JsonPropertyName("first_air_date")]
     public string? FirstAirDate { get; set; }
+
+    [JsonPropertyName("video")]
     public bool? Video { get; set; }
+
+    [JsonPropertyName("vote_average")]
     public double VoteAverage { get; set; }
+
+    [JsonPropertyName("vote_count")]
     public int VoteCount { get; set; }
-    public List<string>? OriginCountry { get; set; }
 }
