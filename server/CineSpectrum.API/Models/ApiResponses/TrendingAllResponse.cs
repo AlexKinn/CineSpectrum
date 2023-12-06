f2d693dac6db3703b9f14d5b97580d2ea8f739c9
AlexKinn <alex.kn@live.com>
2023-12-06 16:03:01 +0000

    create model for external API response

---
+++ server/CineSpectrum.API/Models/ApiResponses/TrendingAllResponse.cs
@@ -0,0 +1,17 @@
+﻿using CineSpectrum.API.Models.DTOs;
+using System.Text.Json.Serialization;
+
+namespace CineSpectrum.API.Models.ApiResponses;
+
+public class TrendingAllResponse
+{
+    [JsonPropertyName("page")]
+    public int Page { get; set; }
+
+    [JsonPropertyName("results")]
+    public List<MediaExternalApiDto> Results { get; set; }
+    [JsonPropertyName("total_pages")]
+    public int TotalPages { get; set; }
+    [JsonPropertyName("total_results")]
+    public int TotalResults { get; set; }
+}
