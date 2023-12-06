6d5a3719c28747680ddc37307a79057efb28518f
AlexKinn <alex.kn@live.com>
2023-12-06 16:04:32 +0000

    setup Tmdb HttpClient

--- server/CineSpectrum.API/Program.cs
+++ server/CineSpectrum.API/Program.cs
@@ -1,5 +1,8 @@
+using CineSpectrum.API.Configurations;
 using CineSpectrum.API.Data;
 using CineSpectrum.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MediaContext>();
 builder.Services.AddScoped<IMediaService, MediaService>();
+builder.Services.AddScoped<IMediaRepository, MediaRepository>();
+builder.Services.Configure<TmdbOptions>(
+    builder.Configuration.GetSection(nameof(TmdbOptions)));
+builder.Services.AddHttpClient("Tmdb", (serviceProvider, client) =>
+{
+    var settings = serviceProvider
+        .GetRequiredService<IOptions<TmdbOptions>>().Value;
+
+    client.DefaultRequestHeaders.Add("accept", "application/json");
+    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {Environment.GetEnvironmentVariable("TMDB_API_KEY")}");
+    //client.BaseAddress = new Uri(settings.BaseURL);
+});
 
 var app = builder.Build();
 
@@ -20,6 +35,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("urls", (IOptions<TmdbOptions> options) =>
+{
+    var response = new
+    {
+        BaseUrl = options.Value.BaseURL,
+        TrendingMediaURL = options.Value.TrendingMediaURL
+    };
+    return Results.Ok(response);
+});
+
+app.MapGet("TmdbKey", () =>
+{
+    var response = new
+    {
+        ApiKey = $"Bearer {Environment.GetEnvironmentVariable("TMDB_API_KEY")}"
+    };
+    return Results.Ok(response);
+});
 
 app.UseHttpsRedirection();
 
