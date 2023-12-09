c426386cd3a64658e0dd1460b5c5075ac10b7538
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-09 01:53:14 +0000

    Create TvShowController and GetTvShowById route

---
+++ server/CineSpectrum.API/Controllers/TvShowController.cs
@@ -0,0 +1,28 @@
+﻿using CineSpectrum.API.Models.DTOs;
+using CineSpectrum.API.Services.TvShow;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineSpectrum.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TvShowController : ControllerBase
+{
+    private readonly ITvShowService _tvShowService;
+    public TvShowController(ITvShowService tvShowService)
+    {
+        _tvShowService = tvShowService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTvShowById(int id)
+    {
+        TvShowExternalApiDto? tvShow = await _tvShowService.GetTvShowById(id); 
+
+        if(tvShow == null)
+        {
+            return NotFound();
+        }
+        return Ok(tvShow);
+    }
+}
