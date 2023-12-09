943ce530e6cddd088531e5ee3344b47a0220fa39
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-09 01:48:36 +0000

    comment out mock files

--- app/src/mocks/handlers.ts
+++ app/src/mocks/handlers.ts
@@ -1,19 +1,19 @@
-import { rest } from 'msw';
-import fs from 'fs';
-import path from 'path';
+// import { rest } from 'msw';
+// import fs from 'fs';
+// import path from 'path';
 
-const dataFilePath = path.join(__dirname, "data.json");
-const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
+// const dataFilePath = path.join(__dirname, "data.json");
+// const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
 
-export const handlers = [
-    rest.get((process.env.REACT_APP_API_URL ?? "http://localhost:8080") +  "/trendingMedia", (req, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json(data.response)
-        )
-    })
-];
+// export const handlers = [
+//     rest.get((process.env.REACT_APP_API_URL ?? "http://localhost:8080") +  "/trendingMedia", (req, res, ctx) => {
+//         return res(
+//             ctx.status(200),
+//             ctx.json(data.response)
+//         )
+//     })
+// ];
 
-const mockData = {
+// const mockData = {
     
-}
+// }
