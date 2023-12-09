5a77c8513047ebfb2275cf88b0883276ccd10630
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-09 01:50:16 +0000

    create IMovieRepository and implementation

---
+++ server/CineSpectrum.API/Services/Movie/IMovieRepository.cs
@@ -0,0 +1,8 @@
+﻿using CineSpectrum.API.Models.DTOs;
+
+namespace CineSpectrum.API.Services.Movie;
+
+public interface IMovieRepository
+{
+    Task<MovieExternalApiDto?> GetMovieById(int id);
+}
---
+++ server/CineSpectrum.API/Services/Movie/MovieRepository.cs
@@ -0,0 +1,36 @@
+﻿using CineSpectrum.API.Configurations;
+using CineSpectrum.API.Models.DTOs;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+using System.Text.Json;
+
+namespace CineSpectrum.API.Services.Movie;
+
+public class MovieRepository : IMovieRepository
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IOptions<TmdbOptions> _options;
+
+    public MovieRepository(IHttpClientFactory httpClientFactory, IOptions<TmdbOptions> options)
+    {
+        _httpClientFactory = httpClientFactory;
+        _options = options;
+    }
+
+    public async Task<MovieExternalApiDto?> GetMovieById(int id)
+    {
+        HttpClient client = _httpClientFactory.CreateClient("Tmdb");
+
+        HttpResponseMessage response = await client.GetAsync($"movie/{id}?language=en-US");
+
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            MovieExternalApiDto? movie = JsonSerializer.Deserialize<MovieExternalApiDto>(json);
+            return movie;
+        }
+        // ERROR HANDLING NEEDED
+        throw new Exception($"Bad response from GetTrendingMedia API call: {response.ReasonPhrase}");
+    }
+}
+ 
