02642d806ff2ec4703debe03dc90852cc37d156c
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-09 01:51:45 +0000

    create ITvShowService and implementation

---
+++ server/CineSpectrum.API/Services/TvShow/TvShowService.cs
@@ -0,0 +1,18 @@
+﻿using CineSpectrum.API.Models.DTOs;
+
+namespace CineSpectrum.API.Services.TvShow;
+
+public class TvShowService : ITvShowService
+{
+    private readonly ITvShowRepository _repository;
+    public TvShowService(ITvShowRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<TvShowExternalApiDto?> GetTvShowById(int id)
+    {
+        TvShowExternalApiDto? tvShowExternalApiDto = await _repository.GetTvShowById(id);
+        return tvShowExternalApiDto;
+    }
+}
