a2a2c301da92916a340fd00ee1810e085b818866
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-09 01:49:47 +0000

    create MovieController

---
+++ server/CineSpectrum.API/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿
+using CineSpectrum.API.Models.DTOs;
+using CineSpectrum.API.Services.Movie;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineSpectrum.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private readonly IMovieService _movieService;
+    public MovieController(IMovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetMovieById(int id)
+    {
+        MovieExternalApiDto? movie = await _movieService.GetMovieById(id);
+
+        if(movie == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(movie);
+    }
+    
+}
