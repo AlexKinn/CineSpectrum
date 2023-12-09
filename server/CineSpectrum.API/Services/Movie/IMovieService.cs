a94f6da355370cd395e9d6e33ca9d383a61d3d1a
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-09 01:50:28 +0000

    setup IMovieService and implementation

---
+++ server/CineSpectrum.API/Services/Movie/IMovieService.cs
@@ -0,0 +1,8 @@
+﻿using CineSpectrum.API.Models.DTOs;
+
+namespace CineSpectrum.API.Services.Movie;
+
+public interface IMovieService
+{
+    Task<MovieExternalApiDto?> GetMovieById(int id);
+}
---
+++ server/CineSpectrum.API/Services/Movie/MovieService.cs
@@ -0,0 +1,18 @@
+﻿using CineSpectrum.API.Models.DTOs;
+
+namespace CineSpectrum.API.Services.Movie;
+
+public class MovieService : IMovieService
+{
+    private readonly IMovieRepository _repository;
+    public MovieService(IMovieRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<MovieExternalApiDto?> GetMovieById(int id)
+    {
+        MovieExternalApiDto? movie = await _repository.GetMovieById(id);
+        return movie;
+    }
+}
