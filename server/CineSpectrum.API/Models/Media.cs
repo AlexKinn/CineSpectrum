6c234eea86e9143b937b3d0094917581a494799c
AlexKinn <alex.kn@live.com>
2023-12-13 13:21:50 +0000

    create Media model

--- server/CineSpectrum.API/Models/Media.cs
+++ server/CineSpectrum.API/Models/Media.cs
@@ -1,6 +1,36 @@
-﻿namespace CineSpectrum.API.Models;
+﻿using System.Text.Json.Serialization;
 
-public class Media
+namespace CineSpectrum.API.Models;
+
+public class MediaExternalApiDto
 {
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
 
+    [JsonPropertyName("media_type")]
+    public string MediaType { get; set; } = string.Empty;
+
+    [JsonPropertyName("backdrop_path")]
+    public string BackdropPath { get; set; } = string.Empty;
+
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("overview")]
+    public string Overview { get; set; } = string.Empty;
+
+    [JsonPropertyName("tagline")]
+    public string Tagline { get; set; } = string.Empty;
+
+    [JsonPropertyName("poster_path")]
+    public string PosterPath { get; set; } = string.Empty;
+
+    [JsonPropertyName("vote_average")]
+    public double VoteAverage { get; set; }
+
+    [JsonPropertyName("vote_count")]
+    public int VoteCount { get; set; }
 }
