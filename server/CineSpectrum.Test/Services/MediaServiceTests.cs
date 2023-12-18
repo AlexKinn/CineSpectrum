fa44e8193adcee6e94c6f974ee0f679c3d09ee44
AlexKinn <65352285+AlexKinn@users.noreply.github.com>
2023-12-18 12:24:25 +0000

    create MediaServiceTests

---
+++ server/CineSpectrum.Test/Services/MediaServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using CineSpectrum.API.Services;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineSpectrum.Tests.Services;
+
+public class MediaServiceTests
+{
+    private readonly IMediaRepository _mediaRepository;
+    public MediaServiceTests()
+    {
+        _mediaRepository = A.Fake<IMediaRepository>();
+    }
+
+    [Fact]
+    public void MediaService_GetTrendingMedia_ReturnsList()
+    {
+
+    }
+}
